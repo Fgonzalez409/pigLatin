f1b0ee2faee136d2ca3a49b4d5bd3ccda0e3fb9c
Fgonzalez409 <fergonz409@gmail.com>
2023-04-07 23:43:46 -0500

    first commit

--- main.js
+++ main.js
@@ -1,43 +1,29 @@
 
 
 
-const form = document.getElementById("form")
-form.addEventListener("submit", (e) => {
-  e.preventDefault();  
-  pigLatin()
-}) 
 
-function fun(){
-  console.log("first")
-}
 
-const body = document.getElementById("body")
-const btn = document.getElementById("button")
-const h2 = document.createElement("h2")
-body.appendChild(h2)
-btn.addEventListener("click", () =>{
-  fun()
-})
+  //gets user input with no whitespace and makes all lowercase
+  let input = document.getElementById("user-input").value.trim().toLowerCase() //user input
 
-const input = document.getElementById("user-input")
+  let output = document.getElementById("output")
 
-const pigLatin = () => {
-  let word = input.value
-  let vowel = ['a','e','i','o','u']
-  let newString = ""
-  h2.innerText = word
-  //if string begins with a vowel
-  if(vowel.indexOf(word[0]) > -1){
-    newString = word.toLowerCase().trim() + "yay"
-    return newString
+
+  function pigLatin(word){
+
+    let vowel = ['a','e','i','o','u']
+    //if string begins with a vowel
+    if(vowel.indexOf(word[0]) > -1){
+      word = word + "yay"
+    }
+    else {
+      let firstMatch = word.match(/[aeiou]/g) || 0
+      let vowel = word.indexOf(firstMatch[0])
+      word = word.substring(vowel) + word.substring(0,vowel) + "ay"
+    }
+
+    output.textContent = pigLatin(input)
+
   }
-
-  else {
-    let firstMatch = word.match(/[aeiou]/g) || 0
-    let vowel = word.indexOf(firstMatch[0])
-    newString = word.substring(vowel).toLowerCase().trim() + word.substring(0,vowel).toLowerCase().trim() + "ay"
-    return newString   
-  }
-  // Your code here
+ 
   
-}
