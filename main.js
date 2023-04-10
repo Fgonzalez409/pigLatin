eed59bb1e829f4cd6d5c4f08878569610be1d443
Fgonzalez409 <fergonz409@gmail.com>
2023-04-09 19:24:48 -0500

    added functionality to print pigLatinized words to DOM

--- main.js
+++ main.js
@@ -1,28 +1,24 @@
+ 
 
 
+  function pigLatin(){
+     //gets user input with no whitespace and makes all lowercase
+    let input = document.getElementById("user-input").value.trim().toLowerCase() //user input
 
-
-
-  //gets user input with no whitespace and makes all lowercase
-  let input = document.getElementById("user-input").value.trim().toLowerCase() //user input
-
-  let output = document.getElementById("output")
-
-
-  function pigLatin(word){
-
+    let output = document.getElementById("output")
+    
     let vowel = ['a','e','i','o','u']
     //if string begins with a vowel
-    if(vowel.indexOf(word[0]) > -1){
-      word = word + "yay"
+    if(vowel.indexOf(input[0]) > -1){
+      input = input + "yay"
     }
     else {
-      let firstMatch = word.match(/[aeiou]/g) || 0
-      let vowel = word.indexOf(firstMatch[0])
-      word = word.substring(vowel) + word.substring(0,vowel) + "ay"
+      let firstMatch = input.match(/[aeiou]/g) || 0
+      let vowel = input.indexOf(firstMatch[0])
+      input = input.substring(vowel) + input.substring(0,vowel) + "ay"
     }
 
-    output.textContent = pigLatin(input)
+    output.innerHTML = input
 
   }
  
